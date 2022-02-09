52144a785c60043b055c99ca4751747b3b4e2cf9
funkyblues <dnr8874@naver.com>
2022-02-09 22:53:25 +0900

    vue3-start

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2,
+    }
+  },
+  beforeCreate() {
+    console.log('Before Create!', this.count) // Before Create! undefined
+  },
+  created() {
+    console.log('Created!', this.count) // Created! 2
+    console.log(document.querySelector('h1')) // null. HTML구조와 연결되기 전 상태이기 때문에 검색 불가능
+  },
+  beforeMount() {
+    console.log('Before Mount!') // Before Mount!
+    console.log(document.querySelector('h1')) // null. HTML구조와 연결되기 전 상태이기 때문에 검색 불가능
+  },
+  mounted() {
+    console.log('Mounted!') // Mounted!
+    console.log(document.querySelector('h1')) //<h1>2</h1>
+  },
+}
+</script>
---
+++ src/components/Fruit.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li>{{ name }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+  h1 {
+    color: red !important;
+  }
+</style>
