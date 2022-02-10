6a3068b7d7ee3cf9d774e84ce061dc243bba4c83
funkyblues <dnr8874@naver.com>
2022-02-11 02:37:34 +0900

    Template Grammar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <h1>{{ count }}</h1>
+  <h1 :[attr]="msg" @[event]="add">{{ msg }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 2,
+      msg: 'active',
+      attr: 'class',
+      event: 'click',
     }
   },
-  beforeCreate() {
-    console.log('Before Create!', this.count) // Before Create! undefined
-  },
-  created() {
-    console.log('Created!', this.count) // Created! 2
-    console.log(document.querySelector('h1')) // null. HTML구조와 연결되기 전 상태이기 때문에 검색 불가능
-  },
-  beforeMount() {
-    console.log('Before Mount!') // Before Mount!
-    console.log(document.querySelector('h1')) // null. HTML구조와 연결되기 전 상태이기 때문에 검색 불가능
-  },
-  mounted() {
-    console.log('Mounted!') // Mounted!
-    console.log(document.querySelector('h1')) //<h1>2</h1>
+  // 속성
+  methods: {
+    add() {
+      this.msg += '!'
+    },
   },
 }
 </script>
+
+<style scoped>
+.active {
+  color: royalblue;
+  font-size: 100px;
+}
+</style>
