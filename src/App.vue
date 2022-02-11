d4511f4a69cd3fa2116530cfc20461eccf35cad4
funkyblues <dnr8874@naver.com>
2022-02-12 02:43:44 +0900

    vuejs - class & style binding

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,29 @@
 <template>
-  <h1 @click="changeMessage">
-    {{ msg }}
+  <h1 
+    :style="[fontStyle, backgroundStyle]"
+    @click="changeStyle">
+    Hello?! 
   </h1>
-  <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: 'Hello?',
+      fontStyle: {
+        color: 'orange',
+        fontSize: '30px'
+      },
+      backgroundStyle: {
+        backgroundColor: 'black'
+      }
     }
   },
-  computed: {
-    reversedMessage() {
-      return this.msg.split('').reverse().join('')
-    },
-  },
-  watch: {
-    msg(newValue) {
-      console.log('msg: ', newValue)
-    },
-    reversedMessage(newValue) {
-      console.log('reversedMessage: ', newValue)
-    },
-  },
   methods: {
-    changeMessage() {
-      this.msg = 'Good!'
-    },
-  },
+    changeStyle() {
+      this.fontStyle.color = 'red',
+      this.fontStyle.fontSize = '50px'
+    }
+  }
 }
 </script>
