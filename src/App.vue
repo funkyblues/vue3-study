bc25f43edf378b9b712191affb5d1cff791e3afa
funkyblues <dnr8874@naver.com>
2022-02-12 00:35:11 +0900

    vuejs - watch

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <button @click="add">ADD</button>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
+  <h1 @click="changeMessage">
+    {{ msg }}
+  </h1>
   <h1>{{ reversedMessage }}</h1>
 </template>
 
@@ -10,25 +9,25 @@
 export default {
   data() {
     return {
-      msg: 'Hello Computed!',
+      msg: 'Hello?',
     }
   },
   computed: {
-    // reversedMessage() {
-    //   return this.msg.split('').reverse().join('')
-    // },
-    reversedMessage: {
-      get() {
-        return this.msg.split('').reverse().join('')
-      },
-      set(value) {
-        this.msg = value
-      },
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    },
+  },
+  watch: {
+    msg(newValue) {
+      console.log('msg: ', newValue)
+    },
+    reversedMessage(newValue) {
+      console.log('reversedMessage: ', newValue)
     },
   },
   methods: {
-    add() {
-      this.reversedMessage += '!?'
+    changeMessage() {
+      this.msg = 'Good!'
     },
   },
 }
