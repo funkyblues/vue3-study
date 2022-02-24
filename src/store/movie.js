885fd238d62d119a3546ffca655e652b97c4fe68
funkyblues <dnr8874@naver.com>
2022-02-24 20:41:02 +0900

    Modify server port number

--- src/store/movie.js
+++ src/store/movie.js
@@ -65,11 +65,8 @@ export default {
             })
           }
         }
-        // error로 받는 객체 데이터를 바로 객체 구조분할을 통해 message로 사용하겠다는 의미입니다.
       } catch ( { message } ) {
         commit('updateState', {
-          // 현재 _fetchMovie가 netlify의 serverless 함수와 연결되어 동작하고 있기 떄문에,
-          // 네트워크로 주고 받은 객체 데이터의 속성을 가져와서 할당해 주어야 합니다.
           movies: [],
           message
         })
@@ -110,12 +107,6 @@ export default {
 }
 
 async function _fetchMovie(payload) {
-  // 이젠 POST Method로 데이터를 넘겨주어야 함
-  // axios의 post 메서드를 통해 serverless 함수 주소로 특정 데이터를 전송
-  // get : 쿼리스트링에 정보를 담아서 클라이언트에서 서버로 요청하는 것
-  // post : payload에 담아서 전송(?). 서버에서 클라이언트로 보내주는 것
-
-  // 비동기로 동작하는 개념
   return await axios.post('/.netlify/functions/movie', payload)
 }
 
