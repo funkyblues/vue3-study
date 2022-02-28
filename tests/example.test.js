3a170b7f92d9454000fe65bca0f125f405247fa7
funkyblues <dnr8874@naver.com>
2022-03-01 01:04:53 +0900

    그래 천천히 하는거도 방법인거 같아

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,31 +1,10 @@
-import { double } from './example'
+import * as example from "./example";
 
-describe('그룹1', () => {
-  beforeAll(() => {
-    console.log('beforeAll!')
-  })
-  afterAll(() => {
-    console.log('afterAll!')
-  })
-  beforeEach(() => {
-    console.log('beforeEach!')
-  })
-  afterEach(() => {
-    console.log('afterEach!')
-  })
-  test('첫 테스트', () => {
-    console.log('첫 테스트!')
-    expect(123).toBe(123)
-  })
-  
-  test('인수가 숫자 데이터입니다.', () => {
-    console.log('인수가 숫자 데이터입니다.')
-    expect(double(3)).toBe(6)
-    expect(double(10)).toBe(20)
-  })
-  
-  test('인수가 없습니다.', () => {
-    console.log('인수가 없습니다.')
-    expect(double()).toBe(0)
-  })
+describe('비동기 테스트', () => {
+  test('async/await', async () => {
+    jest.spyOn(example, 'asyncFn')
+      .mockResolvedValue('Done?')
+    const res = await example.asyncFn()
+    expect(res).toBe('Done!')
+  }, 7000)
 })
